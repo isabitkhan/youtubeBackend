e94f61a38c6a0aa52df9dd940d26889344082ed7
Sabit Khan <mesabitkhan@gmail.com>
2024-06-14 19:26:22 +0500

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { auth } from "../middlewares/auth.middleware.js";
@@ -23,5 +30,14 @@ router.route("/login").post(loginUser);
 //Secure routes
 router.route("/logout").post(auth, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(auth, changeCurrentPassword);
+router.route("/current-user").get(auth, getCurrentUser);
+router.route("/update-account").patch(auth, updateAccountDetails);
+router.route("/avatar").patch(auth, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(auth, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(auth, getUserChannelProfile);
+router.route("/history").get(auth, getWatchHistory);
 
 export default router;
